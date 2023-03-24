a35b1476cda6aaea9ec848fa7bce27e9f110b812
Slipninja <26084911+SlipNinja@users.noreply.github.com>
2023-03-24 12:09:36 +0100

    Add header content and navigation bar

--- src/pageBuilder.js
+++ src/pageBuilder.js
@@ -1,3 +1,5 @@
+import eggs from './images/eggs.jpg';
+import marmite from './images/marmite.png';
 
 function newElement(type, id) {
     const newElt = document.createElement(type);
@@ -9,11 +11,43 @@ const buildPage = (element) => {
     console.log("Website under construction...")
 
     const header = newElement("div", "header");
-    const mainContent = newElement("div", "mainContent");
+    const centralContent = newElement("div", "centralContent");
+    const navigation = newElement("div", "navigation");
+    const modularContent = newElement("div", "modularContent");
     const footer = newElement("div", "footer");
 
+    // HEADER
+
+    const logo = new Image();
+    logo.src = marmite;
+    logo.classList.add("logo");
+
+    const title = document.createElement("h1");
+    title.innerHTML = "The Eggxellent";
+
+    header.appendChild(logo);
+    header.appendChild(title);
+
+    // MODULARCONTENT ( To be externalised in modules ! )
+
+    /*
+    const mainImage = new Image();
+    mainImage.src = eggs;
+    mainImage.classList.add("main-image");
+
+    modularContent.appendChild(mainImage);
+    */
+
+    // FOOTER
+
+
+    // Structural linking
+
+    centralContent.appendChild(navigation);
+    centralContent.appendChild(modularContent);
+
     element.appendChild(header);
-    element.appendChild(mainContent);
+    element.appendChild(centralContent);
     element.appendChild(footer);
 };
 
